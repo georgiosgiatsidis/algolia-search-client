a18a9688164d796506a52e3bff1455fffa31128e
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-09-05 12:04:54 +0300

    Setup env variables command

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const importJsx = require('import-jsx');
 const React = require('react');
 const cli = require('commander');
+const fs = require('fs');
 const inquirer = require('inquirer');
 const { render } = require('ink');
 const pkg = require('../package.json');
-require('./config');
 
 /**
  * Run the application
@@ -31,6 +31,19 @@ cli.command('search').action(async () => {
     ]);
     run(query);
 });
+
+cli.command('setup')
+    .requiredOption('-a, --applicationId <id>', 'The application ID')
+    .requiredOption('-k, --key <key>', 'The API Key')
+    .requiredOption('-i, --index <index>', 'The index name')
+    .action((cmd) => {
+        const envVars = [`APPLICATION_ID=${cmd.applicationId}`, `API_KEY=${cmd.key}`, `INDEX_NAME=${cmd.index}`];
+
+        fs.writeFile(`${process.cwd()}/.env`, envVars.join('\n'), (err) => {
+            if (err) return console.error(err);
+            return true;
+        });
+    });
 
 cli.version(pkg.version, '-v, --version');
 
